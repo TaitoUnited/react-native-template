2ec64d3416bc84218ba948b27e9bdad664177d29
Julien Texier <julien.texier@taitounited.fi>
2022-06-20 13:49:46 +0300

    removing more helcast related urls

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,10 +3,8 @@ import Config from 'react-native-config';
 const config = {
   APP_ENV: Config.APP_ENV,
   API_URL: Config.API_URL,
-  TERMS_OF_SERVICE_URL:
-    'https://faces.taitotemplate.com/henkilotietojen-kasittely-ja-palvelun-kayttoehdot/',
-  PRIVACY_POLICY_URL:
-    'https://faces.taitotemplate.com/henkilotietojen-kasittely-ja-palvelun-kayttoehdot/',
+  TERMS_OF_SERVICE_URL: 'https://en.wikipedia.org/wiki/Terms_of_service', // TODO: replace with real url
+  PRIVACY_POLICY_URL: 'https://en.wikipedia.org/wiki/Privacy_policy/', // TODO: replace with real url
   PLACEHOLDER_MODE: Config.PLACEHOLDER_MODE === 'true',
 };
 
