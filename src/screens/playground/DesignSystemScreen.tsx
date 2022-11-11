e470fc799d78fe38df3117f837fba6c7fb6e289a
Teemu Taskula <themetor@gmail.com>
2022-11-11 22:46:02 +0200

    feat: add typography, radii, and spacing examples to playground

--- src/screens/playground/DesignSystemScreen.tsx
+++ src/screens/playground/DesignSystemScreen.tsx
@@ -5,6 +5,9 @@ import { Color, styled, themeProp } from '~styles';
 import { Grid, Stack, Text } from '~components/uikit';
 import { ScreenProps } from '~screens/types';
 import { light as colors } from '~design-system/colors';
+import { native as typography } from '~design-system/typography';
+import * as radii from '~design-system/radii';
+import * as spacing from '~design-system/spacing';
 
 const otherColors: Color[] = [];
 const colorNames = Object.keys(colors).sort() as Color[];
@@ -17,6 +20,10 @@ Object.keys(colorGroups).forEach((group) => {
     delete colorGroups[group];
   }
 });
+
+const typographyNames = Object.keys(typography).sort();
+const radiiEntries = Object.entries(radii).sort((a, b) => a[1] - b[1]);
+const spacingEntries = Object.entries(spacing).sort((a, b) => a[1] - b[1]);
 
 export default function DesignSystemScreen(_: ScreenProps<'DesignSystem'>) {
   return (
@@ -56,17 +63,49 @@ export default function DesignSystemScreen(_: ScreenProps<'DesignSystem'>) {
 
         <Stack axis="y" spacing="medium">
           <Text variant="title2">Typography</Text>
-          <Text variant="body">TODO</Text>
+
+          <Stack axis="y" spacing="xsmall">
+            {typographyNames.map((name) => (
+              <TypographyBlock key={name}>
+                <Text variant={name as any}>{startCase(name)}</Text>
+              </TypographyBlock>
+            ))}
+          </Stack>
         </Stack>
 
-        <Stack axis="y" spacing="medium">
+        <Stack axis="y" spacing="normal">
           <Text variant="title2">Radii</Text>
-          <Text variant="body">TODO</Text>
+
+          <Grid spacing="normal" justify="center">
+            {radiiEntries.map(([name, value]) => (
+              <Stack key={name} axis="y" spacing="xsmall" align="center">
+                <RadiiBlock style={{ borderRadius: value }}>
+                  <Text variant="body" color="textMuted">
+                    {value}px
+                  </Text>
+                </RadiiBlock>
+                <Text variant="bodySmall">{startCase(name)}</Text>
+              </Stack>
+            ))}
+          </Grid>
         </Stack>
 
         <Stack axis="y" spacing="medium">
           <Text variant="title2">Spacing</Text>
-          <Text variant="body">TODO</Text>
+
+          <Stack axis="y" spacing="xxsmall">
+            {spacingEntries.map(([name, value]) => (
+              <Stack key={name} axis="x" spacing="xsmall" align="center">
+                <Text variant="bodySmall" style={{ minWidth: 56 }}>
+                  {startCase(name)}
+                </Text>
+                <SpacingBlock key={name} style={{ width: value }} />
+                <Text variant="bodySmall" color="textMuted">
+                  {value}px
+                </Text>
+              </Stack>
+            ))}
+          </Stack>
         </Stack>
       </Stack>
     </Wrapper>
@@ -78,6 +117,7 @@ const Wrapper = styled('ScrollView', {
 }).attrs((p) => ({
   contentContainerStyle: {
     padding: p.theme.space.normal,
+    paddingBottom: 200,
   },
 }));
 
@@ -92,4 +132,28 @@ const ColorBlock = styled('View', {
       backgroundColor: color,
     })),
   },
+});
+
+const TypographyBlock = styled('View', {
+  padding: '$normal',
+  borderRadius: '$medium',
+  borderWidth: 1,
+  borderColor: 'rgba(150, 150, 150, 0.15)',
+});
+
+const RadiiBlock = styled('View', {
+  height: 100,
+  width: 100,
+  borderWidth: 1,
+  borderColor: '$border',
+  backgroundColor: '$muted5',
+  flexCenter: 'row',
+});
+
+const SpacingBlock = styled('View', {
+  height: 24,
+  borderWidth: 1,
+  borderRadius: 2,
+  borderColor: '$border',
+  backgroundColor: '$muted5',
 });
