e90fb7324cee4b6d7b2bb77112efe7a779f7ee3c
Teemu Taskula <themetor@gmail.com>
2022-11-11 17:36:23 +0200

    feat: add more button examples

--- src/screens/playground/ButtonsScreen.tsx
+++ src/screens/playground/ButtonsScreen.tsx
@@ -19,6 +19,7 @@ export default function ButtonsScreen(_: ScreenProps<'Buttons'>) {
       <Stack axis="y" spacing="large">
         <Stack axis="y" spacing="small">
           <Text variant="overline">Button sizes</Text>
+
           <FillButton variant="neutral" size="large" onPress={handlePress}>
             Large
           </FillButton>
@@ -42,36 +43,53 @@ export default function ButtonsScreen(_: ScreenProps<'Buttons'>) {
 
         <Stack axis="y" spacing="small">
           <Text variant="overline">Button variants</Text>
+
           <FillButton variant="neutral" onPress={handlePress}>
             Neutral
           </FillButton>
+          <OutlineButton variant="neutral" onPress={handlePress}>
+            Neutral
+          </OutlineButton>
           <FillButton variant="primary" onPress={handlePress}>
             Primary
           </FillButton>
+          <OutlineButton variant="primary" onPress={handlePress}>
+            Primary
+          </OutlineButton>
           <FillButton variant="info" onPress={handlePress}>
             Info
           </FillButton>
+          <OutlineButton variant="info" onPress={handlePress}>
+            Info
+          </OutlineButton>
           <FillButton variant="warn" onPress={handlePress}>
             Warning
           </FillButton>
+          <OutlineButton variant="warn" onPress={handlePress}>
+            Warning
+          </OutlineButton>
           <FillButton variant="danger" onPress={handlePress}>
             Danger
           </FillButton>
+          <OutlineButton variant="danger" onPress={handlePress}>
+            Danger
+          </OutlineButton>
         </Stack>
 
         <Stack axis="y" spacing="small">
           <Text variant="overline">Button icons</Text>
+
           <FillButton variant="neutral" icon="camera" onPress={handlePress}>
             Next to label
           </FillButton>
-          <FillButton
+          <OutlineButton
             variant="neutral"
             icon="download"
             iconSide="start"
             onPress={handlePress}
           >
             Next to label
-          </FillButton>
+          </OutlineButton>
           <FillButton
             variant="neutral"
             icon="calendar"
@@ -80,7 +98,7 @@ export default function ButtonsScreen(_: ScreenProps<'Buttons'>) {
           >
             At the edge
           </FillButton>
-          <FillButton
+          <OutlineButton
             variant="neutral"
             icon="bluetoothFilled"
             iconSide="start"
@@ -88,17 +106,34 @@ export default function ButtonsScreen(_: ScreenProps<'Buttons'>) {
             onPress={handlePress}
           >
             At the edge
-          </FillButton>
+          </OutlineButton>
         </Stack>
 
         <Stack axis="y" spacing="normal">
           <Text variant="overline">Icon buttons</Text>
+
+          <Stack axis="x" spacing="medium">
+            <IconButton size="large" icon="languageGlobe" color="text" />
+            <IconButton size="large" icon="eyeFilled" color="primary" />
+            <IconButton size="large" icon="fingerPrint" color="info" />
+            <IconButton size="large" icon="x" color="error" />
+            <IconButton size="large" icon="bell" color="warnText" />
+          </Stack>
+
           <Stack axis="x" spacing="medium">
             <IconButton icon="languageGlobe" color="text" />
             <IconButton icon="eyeFilled" color="primary" />
             <IconButton icon="fingerPrint" color="info" />
             <IconButton icon="x" color="error" />
             <IconButton icon="bell" color="warnText" />
+          </Stack>
+
+          <Stack axis="x" spacing="medium">
+            <IconButton size="small" icon="languageGlobe" color="text" />
+            <IconButton size="small" icon="eyeFilled" color="primary" />
+            <IconButton size="small" icon="fingerPrint" color="info" />
+            <IconButton size="small" icon="x" color="error" />
+            <IconButton size="small" icon="bell" color="warnText" />
           </Stack>
         </Stack>
       </Stack>
