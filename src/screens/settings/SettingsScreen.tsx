9ef20e4c13c0247f1ca88041fe95ee6306746da2
Teemu Taskula <themetor@gmail.com>
2022-11-15 16:04:17 +0200

    feat: allow opening playground from settings screen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,20 +1,39 @@
+import { Trans } from '@lingui/macro';
+
 import { styled } from '~styles';
 import { FillButton, Stack } from '~components/uikit';
-import { useI18n } from '~services/i18n';
-import LogoutButton from '~components/auth/LogoutButton';
-import ColorModeButton from '~components/common/ColorModeButton';
 import { ScreenProps } from '~screens/types';
+import { useI18n } from '~services/i18n';
+import { useColorMode } from '~services/color-mode';
+import LogoutButton from '~components/auth/LogoutButton';
+import { usePlaygroundStore } from '~screens/playground/helpers';
 
 export default function SettingsScreen(_: ScreenProps<'Settings'>) {
   const { toggleLocale, locale } = useI18n();
+  const { toggleColorMode, colorMode } = useColorMode();
+  const setPlaygroundVisible = usePlaygroundStore(
+    (s) => s.setPlaygroundVisible
+  );
+
+  const colorModeLabel =
+    colorMode === 'light' ? <Trans>dark</Trans> : <Trans>light</Trans>;
 
   return (
     <Wrapper>
-      <Stack axis="y" spacing="large" align="center">
+      <Stack axis="y" spacing="normal">
         <FillButton variant="neutral" onPress={toggleLocale}>
-          {locale}
+          <Trans>Change language to</Trans>{' '}
+          {locale === 'en' ? 'Finnish' : 'englanti'}
         </FillButton>
-        <ColorModeButton />
+        <FillButton variant="neutral" onPress={toggleColorMode}>
+          <Trans>Change theme to {colorModeLabel} mode</Trans>
+        </FillButton>
+        <FillButton
+          variant="neutral"
+          onPress={() => setPlaygroundVisible(true)}
+        >
+          <Trans>Open playground</Trans>
+        </FillButton>
         <LogoutButton />
       </Stack>
     </Wrapper>
