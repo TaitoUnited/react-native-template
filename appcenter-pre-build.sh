53618d680dec8a4522c91b9025dded24f5d09707
Teemu Taskula <themetor@gmail.com>
2022-11-18 21:20:41 +0200

    build: fail prod ci build if translations are missing

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,7 +5,12 @@ echo "Pre-build script start"
 # -----------------------------------------------------------------------------
 echo "Compiling language files..."
 npm run i18n:extract
-npm run i18n:compile
+
+if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "main" ]; then
+  npm run i18n:compile:strict
+else
+  npm run i18n:compile
+fi
 # -----------------------------------------------------------------------------
 
 echo "Pre-build script end"
