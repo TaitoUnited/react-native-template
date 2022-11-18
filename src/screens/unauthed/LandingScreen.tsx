42542382478172ce0397bcd7ea44ff86656b38a6
Teemu Taskula <themetor@gmail.com>
2022-11-18 15:48:45 +0200

    feat: update landing screen

--- src/screens/unauthed/LandingScreen.tsx
+++ src/screens/unauthed/LandingScreen.tsx
@@ -1,44 +1,158 @@
-import { Trans } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
+import { StatusBar, useWindowDimensions } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import * as DropdownMenu from 'zeego/dropdown-menu';
 
-import { styled } from '~styles';
-import { Stack, FillButton } from '~components/uikit';
+import { styled, useTheme } from '~styles';
+import { IconButton, Stack, Text } from '~components/uikit';
 import { ScreenProps } from '~screens/types';
 import { useI18n } from '~services/i18n';
 
 export default function LandingScreen({ navigation }: ScreenProps<'Landing'>) {
-  const { toggleLocale, locale } = useI18n();
+  const { height } = useWindowDimensions();
+  const insets = useSafeAreaInsets();
+  const theme = useTheme();
 
   return (
     <Wrapper>
-      <SafeArea>
-        <Stack axis="y" spacing="normal">
-          <FillButton
-            variant="neutral"
-            onPress={() => navigation.navigate('Login')}
-          >
-            <Trans>Login</Trans>
-          </FillButton>
-          <FillButton
-            variant="neutral"
-            onPress={() => navigation.navigate('Signup')}
-          >
-            <Trans>Signup</Trans>
-          </FillButton>
-          <FillButton variant="neutral" onPress={toggleLocale}>
-            {locale}
-          </FillButton>
-        </Stack>
-      </SafeArea>
+      <ImageBackground source={require('./landing_background.jpg')}>
+        <TopSection
+          style={{ paddingTop: Math.max(insets.top, theme.space.normal) }}
+        >
+          <TopSectionHeader>
+            <LanguageSelector />
+          </TopSectionHeader>
+
+          <TopSectionBody>
+            <Stack axis="y" spacing="medium">
+              <BlackText variant="title3" align="center" withLineHeight>
+                <Trans>Welcome to</Trans>
+              </BlackText>
+              <BlackText variant="title1" align="center">
+                <Trans>React Native Template</Trans>
+              </BlackText>
+              <BlackText variant="subtitle" align="center" withLineHeight>
+                <Trans>By Taito United</Trans> 💚
+              </BlackText>
+            </Stack>
+          </TopSectionBody>
+        </TopSection>
+
+        <BottomSection style={{ minHeight: height * 0.4 }}>
+          <Stack axis="y" spacing="normal">
+            <WhiteText variant="body" align="center" withLineHeight>
+              ✨ <Trans>Start your journey</Trans> ✨
+            </WhiteText>
+
+            <Button onPress={() => navigation.navigate('Login')}>
+              <WhiteText variant="bodyBold">
+                <Trans>Sign in</Trans>
+              </WhiteText>
+            </Button>
+
+            <Stack
+              axis="x"
+              spacing="xsmall"
+              align="center"
+              style={{ alignSelf: 'center' }}
+            >
+              <Line />
+              <WhiteText variant="overline">Or</WhiteText>
+              <Line />
+            </Stack>
+
+            <Button onPress={() => navigation.navigate('Signup')}>
+              <WhiteText variant="bodyBold">
+                <Trans>Create an account</Trans>
+              </WhiteText>
+            </Button>
+          </Stack>
+        </BottomSection>
+      </ImageBackground>
+
+      <StatusBar barStyle="light-content" />
     </Wrapper>
   );
 }
 
+function LanguageSelector() {
+  const { setLocale } = useI18n();
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <IconButton icon="languageGlobe" forcedColor="#fff" />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Item key="fi" onSelect={() => setLocale('fi')}>
+          <DropdownMenu.ItemTitle>{t`Finnish`}</DropdownMenu.ItemTitle>
+        </DropdownMenu.Item>
+        <DropdownMenu.Item key="en" onSelect={() => setLocale('en')}>
+          <DropdownMenu.ItemTitle>{t`English`}</DropdownMenu.ItemTitle>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}
+
+// NOTE: it's often the case that the landing screen is very custom and doesn't
+// adhere to the design system 100%. In that case, it's ok to use custom styles
+// that are out of the design system like here we are using hard coded white color.
+
 const Wrapper = styled('View', {
+  position: 'relative',
+  flex: 1,
+  backgroundColor: '#000',
+});
+
+const ImageBackground = styled('ImageBackground', {
+  flex: 1,
+  justifyContent: 'flex-end',
+  paddingHorizontal: '$xxsmall',
+});
+
+const BlackText = styled(Text, {
+  color: 'rgba(0, 0, 0, 0.8)',
+});
+
+const WhiteText = styled(Text, {
+  color: '#fff',
+});
+
+const TopSection = styled('View', {
   flex: 1,
 });
 
-const SafeArea = styled('SafeAreaView', {
+const TopSectionHeader = styled('View', {
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  paddingHorizontal: '$normal',
+});
+
+const TopSectionBody = styled('View', {
   flex: 1,
-  padding: '$large',
   flexCenter: 'column',
+  padding: '$large',
+});
+
+const BottomSection = styled('View', {
+  padding: '$normal',
+  paddingTop: '$large',
+  backgroundColor: 'rgba(0, 0, 0, 0.65)',
+  borderRadius: '$large',
+});
+
+const Button = styled('TouchableHighlight', {
+  padding: '$medium',
+  borderRadius: '$full',
+  backgroundColor: 'rgba(0, 0, 0, 1)',
+  flexCenter: 'row',
+}).attrs(() => ({
+  underlayColor: 'rgba(0, 0, 0, 0.6)',
+}));
+
+const Line = styled('View', {
+  height: 1,
+  width: 72,
+  backgroundColor: 'rgba(255, 255, 255, 0.15)',
 });
