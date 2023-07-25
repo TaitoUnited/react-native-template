d8761314483c4c2ac50eead43268c10f801494ce
Julien Texier <julien.texier@taitounited.fi>
2023-07-25 12:43:07 +0300

    chore: fixing eas prebuild + expo env

--- post-install.sh
+++ post-install.sh
@@ -6,11 +6,11 @@ echo "Pre-build script start"
 echo "Compiling language files..."
 npm run i18n:extract
 
-# if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "main" ]; then
+if [ "$EXPO_PUBLIC_APP_ENV" == "prod" ] || [ "$EXPO_PUBLIC_APP_ENV" == "stag" ]; then
   npm run i18n:compile:strict
-# else
-  # npm run i18n:compile
-# fi
+else
+  npm run i18n:compile
+fi
 # -----------------------------------------------------------------------------
 
 echo "Pre-build script end"
