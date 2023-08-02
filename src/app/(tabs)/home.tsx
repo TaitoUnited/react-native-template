1f47b995f86cfa3f9203d68c29ef1844768aabed
Julien Texier <julien.texier@taitounited.fi>
2023-08-02 14:38:43 +0300

    chore: adding channels to eas

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,14 +1,39 @@
 import { Trans } from '@lingui/macro';
 
-import { Text } from '~components/uikit';
+import { FillButton, Stack, Text } from '~components/uikit';
 import { styled } from '~styles';
+import * as Updates from 'expo-updates';
+import { Button } from 'react-native';
+import { capitalize } from 'lodash';
+import config from '~constants/config';
 
 export default function Home() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      console.log(`>> Here is the update: ${JSON.stringify(update)}`);
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   return (
     <Wrapper>
-      <Text variant="body">
-        <Trans>Home</Trans>
-      </Text>
+      <Stack axis="y" spacing="normal">
+        <Text variant="body">
+          <Trans>Home</Trans>
+        </Text>
+
+        <Text variant="body">
+          <Trans>Environment: {capitalize(config.appEnv)}</Trans>
+        </Text>
+        <Button title="Fetch update" onPress={onFetchUpdateAsync} />
+      </Stack>
     </Wrapper>
   );
 }
