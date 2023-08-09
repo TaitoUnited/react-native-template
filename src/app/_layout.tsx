18b663b5b639a493b701472635e27df78cd3f8f6
Julien Texier <julien.texier@taitounited.fi>
2023-08-09 11:20:31 +0300

    style: status bar update

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout() {
   return (
     <Providers>
       <RootLayoutNavigator />
-      <StatusBar />
+      <StatusBar transparent />
       {appReady && <RouteProtection />}
     </Providers>
   );
