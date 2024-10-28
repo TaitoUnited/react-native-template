5273f499354a53e45140f2fbe60d2ed5e336b4a4
Julien Texier <julien.texier@taitounited.fi>
2024-10-28 16:23:37 +0200

    feat: add option for customizable bottom bar layout
    - Introduced `CustomBottomBar` component for enhanced customization of bottom tabs (e.g., animations, unique styles)
    - Added `USE_CUSTOM_TABS` flag to toggle between `CustomBottomBar` and `DefaultBottomBar`
    - Included developer notes on when to use the custom bottom bar for client-specific needs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,22 +3,33 @@ import { Tabs } from 'expo-router';
 import { StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { BottomBar } from '~components/common/custom-bottom-bar/BottomBar';
 import StoreReview from '~components/store-review/StoreReview';
 import { Icon, Text } from '~components/uikit';
 import type { IconName } from '~components/uikit/Icon';
 import { useI18n } from '~services/i18n';
 import { useTheme } from '~styles';
 
-type TabList = {
+export type TabList = {
   id: string;
   title: string;
   iconFilled: IconName;
   iconOutlined: IconName;
 }[];
 
+/**
+ * Determines whether to use a fully customizable bottom tab bar (`CustomBottomBar`)
+ * or the default tab layout with minimal customization (`DefaultBottomBar`).
+ *
+ * Set `USE_CUSTOM_TABS` to `true` if the client requires a highly customizable tab bar (animations, unique styling, etc.).
+ * Set it to `false` to use the default tab setup, which applies simpler, standard styling.
+ *
+ * Example: For a custom bottom bar based on specific branding needs, set `USE_CUSTOM_TABS = true`.
+ */
+const USE_CUSTOM_TABS = true;
+
 export default function TabsLayout() {
   const { _ } = useI18n();
-  const insets = useSafeAreaInsets();
 
   const theme = useTheme();
   const tabs: TabList = [
@@ -49,6 +60,27 @@ export default function TabsLayout() {
       iconOutlined: 'settings',
     },
   ];
+
+  return (
+    <>
+      {USE_CUSTOM_TABS ? (
+        <CustomBottomBar tabs={tabs} theme={theme} />
+      ) : (
+        <DefaultBottomBar tabs={tabs} theme={theme} />
+      )}
+      <StoreReview />
+    </>
+  );
+}
+
+type BottomBarProps = {
+  tabs: TabList;
+  theme: ReturnType<typeof useTheme>;
+};
+
+function DefaultBottomBar({ tabs, theme }: BottomBarProps) {
+  const { _ } = useI18n();
+  const insets = useSafeAreaInsets();
 
   function renderTabIcon({
     focused,
@@ -83,48 +115,71 @@ export default function TabsLayout() {
   }
 
   return (
-    <>
-      <Tabs
-        initialRouteName="home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: theme.colors.surface,
-          },
-          headerTintColor: theme.colors.text,
-          headerBackgroundContainerStyle: {
-            borderBottomColor: theme.colors.line3,
-            borderBottomWidth: StyleSheet.hairlineWidth,
-          },
-          headerTitleStyle: {
-            fontSize: theme.fontSizes.bodyBold,
-          },
-          tabBarStyle: {
-            backgroundColor: theme.colors.surface,
-            borderTopColor: theme.colors.line3,
-            borderTopWidth: StyleSheet.hairlineWidth,
-          },
-        }}
-      >
-        {tabs.map(({ id, title, iconFilled, iconOutlined }) => (
-          <Tabs.Screen
-            key={title}
-            name={id}
-            options={{
-              title,
-              tabBarAccessibilityLabel: _(msg`${title} tab`),
-              tabBarItemStyle: {
-                // On certain devices without insets, the tab bar is too close to the bottom of the screen
-                paddingBottom: insets.bottom === 0 ? 4 : 0,
-              },
-              tabBarLabel: ({ focused }) =>
-                renderBarLabel({ focused, id, title }),
-              tabBarIcon: ({ focused }) =>
-                renderTabIcon({ focused, iconFilled, iconOutlined }),
-            }}
-          />
-        ))}
-      </Tabs>
-      <StoreReview />
-    </>
+    <Tabs
+      initialRouteName="home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.surface,
+        },
+        headerTintColor: theme.colors.text,
+        headerBackgroundContainerStyle: {
+          borderBottomColor: theme.colors.line3,
+          borderBottomWidth: StyleSheet.hairlineWidth,
+        },
+        headerTitleStyle: {
+          fontSize: theme.fontSizes.bodyBold,
+        },
+        tabBarStyle: {
+          backgroundColor: theme.colors.surface,
+          borderTopColor: theme.colors.line3,
+          borderTopWidth: StyleSheet.hairlineWidth,
+        },
+      }}
+    >
+      {tabs.map(({ id, title, iconFilled, iconOutlined }) => (
+        <Tabs.Screen
+          key={title}
+          name={id}
+          options={{
+            title,
+            tabBarAccessibilityLabel: _(msg`${title} tab`),
+            tabBarItemStyle: {
+              // On certain devices without insets, the tab bar is too close to the bottom of the screen
+              paddingBottom: insets.bottom === 0 ? 4 : 0,
+            },
+            tabBarLabel: ({ focused }) =>
+              renderBarLabel({ focused, id, title }),
+            tabBarIcon: ({ focused }) =>
+              renderTabIcon({ focused, iconFilled, iconOutlined }),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+}
+
+function CustomBottomBar({ tabs, theme }: BottomBarProps) {
+  return (
+    <Tabs
+      initialRouteName="home"
+      tabBar={(props) => <BottomBar {...props} tabs={tabs} />}
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.surface,
+        },
+        headerTintColor: theme.colors.text,
+        headerBackgroundContainerStyle: {
+          borderBottomColor: theme.colors.line3,
+          borderBottomWidth: StyleSheet.hairlineWidth,
+        },
+        headerTitleStyle: {
+          fontSize: theme.fontSizes.bodyBold,
+        },
+      }}
+    >
+      {tabs.map(({ id, title }) => (
+        <Tabs.Screen key={title} name={id} options={{ title }} />
+      ))}
+    </Tabs>
   );
 }
---
+++ src/components/common/custom-bottom-bar/Tab.tsx
@@ -0,0 +1,101 @@
+import { msg } from '@lingui/macro';
+import { memo, useMemo } from 'react';
+import { Pressable } from 'react-native';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+
+import { type TabList } from '~app/(tabs)/_layout';
+import { Icon, Stack, Text } from '~components/uikit';
+import { useI18n } from '~services/i18n';
+import { styled } from '~styles';
+
+const ANIMATION_DURATION = 350;
+
+type TabBarButtonProps = {
+  onPress: () => void;
+  onLongPress: () => void;
+  isFocused: boolean;
+  label: string;
+  tab: TabList[number];
+};
+
+const TabBarButton = ({
+  isFocused,
+  label,
+  tab,
+  ...pressableProps
+}: TabBarButtonProps) => {
+  const { _ } = useI18n();
+  const iconScale = useSharedValue(isFocused ? 1 : 0);
+  const labelOpacity = useSharedValue(isFocused ? 1 : 0);
+
+  const updateFocusAnimation = (focused: boolean) => {
+    iconScale.value = withSpring(focused ? 1 : 0, {
+      duration: ANIMATION_DURATION,
+    });
+    labelOpacity.value = withSpring(focused ? 1 : 0, {
+      duration: ANIMATION_DURATION,
+    });
+  };
+
+  useMemo(() => updateFocusAnimation(isFocused), [isFocused]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const animatedIconStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: interpolate(iconScale.value, [0, 1], [1, 1.2]) }],
+    top: interpolate(iconScale.value, [0, 1], [0, 8]),
+  }));
+
+  const animatedTextStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(labelOpacity.value, [0, 1], [1, 0]),
+  }));
+
+  return (
+    <Wrapper
+      {...pressableProps}
+      onPressIn={() => (iconScale.value = withTiming(0.8, { duration: 150 }))}
+      onPressOut={() => (iconScale.value = withTiming(1, { duration: 150 }))}
+      accessibilityRole="button"
+      accessibilityLabel={label}
+      accessibilityHint={_(msg`Double tap to select this tab`)}
+    >
+      <Stack axis="y" align="center" spacing="xs">
+        <Animated.View style={animatedIconStyle}>
+          <Icon
+            name={isFocused ? tab.iconFilled : tab.iconOutlined}
+            color={isFocused ? 'primary' : 'neutral2'}
+          />
+        </Animated.View>
+        <LabelWrapper style={animatedTextStyle}>
+          <StyledLabel variant="bodyExtraSmall" isFocused={isFocused}>
+            {label}
+          </StyledLabel>
+        </LabelWrapper>
+      </Stack>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled(Pressable, {
+  flex: 1,
+});
+
+const LabelWrapper = Animated.createAnimatedComponent(styled('View', {}));
+
+const StyledLabel = styled(Text, {
+  variants: {
+    isFocused: {
+      true: { color: '$primary' },
+      false: { color: '$textMuted' },
+    },
+  },
+});
+
+export const TabButton = memo(
+  TabBarButton,
+  (prev, next) => prev.isFocused === next.isFocused
+);
