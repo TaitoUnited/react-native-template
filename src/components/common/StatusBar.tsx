44b02d4b9c49d2e34152096c5a4d5b8a2ea13430
Julien Texier <julien.texier@taitounited.fi>
2025-05-15 14:57:39 +0300

    refactor: simplify StatusBar component by removing deprecated backgroundColor

--- src/components/common/StatusBar.tsx
+++ src/components/common/StatusBar.tsx
@@ -1,17 +1,9 @@
 import { StatusBar as RNStatusBar } from 'expo-status-bar';
 
 import { useColorMode } from '~services/color-mode';
-import { useTheme } from '~styles/styled';
 
 export default function StatusBar({ transparent = false }) {
   const { colorMode } = useColorMode();
-  const theme = useTheme();
 
-  return (
-    <RNStatusBar
-      style={colorMode}
-      translucent={transparent}
-      backgroundColor={transparent ? 'transparent' : theme.colors.surface}
-    />
-  );
+  return <RNStatusBar style={colorMode} translucent={transparent} />;
 }
