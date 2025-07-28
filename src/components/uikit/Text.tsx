6c2757e15608eb8c20b5609b045956f2a75dbca1
Julien Texier <julien.texier@taitounited.fi>
2025-07-28 08:26:13 +0300

    chore: refactor Text component for optimized performance

--- src/components/uikit/Text.tsx
+++ src/components/uikit/Text.tsx
@@ -1,17 +1,48 @@
-import { Text as RNText } from 'react-native';
+import { type ComponentType, createElement, forwardRef } from 'react';
+import type { TextProps as RNTextProps } from 'react-native';
 import { StyleSheet, useUnistyles } from 'react-native-unistyles';
 
 import type * as typographyTokens from '~design-system/typography';
 import { type Color } from '~styles/styled';
 import { getTypography } from '~styles/utils';
 
-type TextProps = RNText['props'] & {
+type TextProps = RNTextProps & {
   variant?: keyof typeof typographyTokens;
   align?: 'left' | 'right' | 'center';
   uppercase?: boolean;
   color?: Color;
   children: React.ReactNode;
 };
+
+const LeanText = forwardRef((props, ref) => {
+  return createElement('RCTText', { ...props, ref });
+}) as ComponentType<TextProps>;
+
+LeanText.displayName = 'LeanText';
+
+export function Text({
+  variant = 'body',
+  align = 'left',
+  uppercase = false,
+  color,
+  children,
+  style,
+  ...props
+}: TextProps) {
+  const { theme } = useUnistyles();
+  const typographyStyle = getTypography(theme, variant);
+
+  styles.useVariants({
+    align,
+    uppercase,
+    color,
+  });
+  return (
+    <LeanText style={[typographyStyle, styles.text, style]} {...props}>
+      {children}
+    </LeanText>
+  );
+}
 
 const styles = StyleSheet.create((theme) => ({
   text: {
@@ -35,25 +66,3 @@ const styles = StyleSheet.create((theme) => ({
     },
   },
 }));
-
-export function Text({
-  variant = 'body',
-  align = 'left',
-  uppercase = false,
-  color,
-  children,
-  ...props
-}: TextProps) {
-  const { theme } = useUnistyles();
-  styles.useVariants({
-    align,
-    uppercase,
-    color,
-  });
-
-  return (
-    <RNText style={[getTypography(theme, variant), styles.text]} {...props}>
-      {children}
-    </RNText>
-  );
-}
